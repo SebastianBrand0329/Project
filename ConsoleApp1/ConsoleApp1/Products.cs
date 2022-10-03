b5a91ebcd14519dd792b02ab8fcd285c67a9cf7d
asistemasm <asistemasm@ASISTENTESISTEM>
2022-10-03 17:28:06 -0500

    Finish Class Products

---
+++ ConsoleApp1/ConsoleApp1/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPOO
+{
+    public abstract class Products
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public float Tax { get; set; }
+
+        public abstract decimal ValueToPay();
+
+        public override string ToString()
+        {
+            return $"{Id}  {Description}" +
+                $"\n\tPrice.....: {$"{Price}",18}" +
+                $"\n\tTax.......: {$"{Tax}",18}";
+        }
+    }
+}
